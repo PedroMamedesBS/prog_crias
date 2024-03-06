9435f7d1c04880ce527abe9d5954e5e53f2eaf7f
Arthur <arthur_rosa2@estudante.sesisenai.org.br>
2024-03-06 08:26:41 -0300

    Criado main

---
+++ formula_main.js
@@ -0,0 +1,37 @@
+document.addEventListener('keydown',(e)=>{
+    
+})
+document.addEventListener('keyup', (e)=>{
+
+})
+
+function game_over(){
+ 
+}
+
+function Pontos(){
+
+}
+
+function colisao(){
+  
+}
+
+
+
+function desenha(){
+
+
+
+}
+
+function atualiza(){
+
+    
+}
+
+function main(){
+
+}
+
+
