2c99e7678064ea4dadf473f036cf27b52eb6b208
Kauê <kaue_f_lima@estudante.sesisenai.org.br>
2024-03-06 08:30:44 -0300

    Criado o class

---
+++ formula_class.js
@@ -0,0 +1,63 @@
+class obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+
+    des_obj(){
+      
+        
+
+    }
+}
+
+
+
+class Carro extends obj{
+    dir = 0
+    pts = 0
+    vida = 5
+
+
+des_carro(){
+        
+
+
+}
+atual_carro(){
+   
+
+}
+point(){
+    
+}
+
+colide(){
+    
+
+}
+}
+class Carro2 extends Carro{
+atual_carro2(){
+    
+    }
+}
+
+
+class Estrada extends obj{
+    des_est(){
+      
+    }
+    mov_est(){
+       
+    }
+}
+
+class Text{
+    des_text(){
+        
+    }
+}
